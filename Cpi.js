d62893821660c4a361114a73e0aa7475de666cb9
Abhi Projects <cool.abhishekerketta@gmail.com>
2020-11-14 11:27:21 +0530

    display rolling dice

--- Cpi.js
+++ Cpi.js
@@ -19,8 +19,10 @@ btnRoll.addEventListener("click", function () {
   //Generation a random number between 1-6
   const dice = Math.floor(Math.random() * 6) + 1;
   console.log(dice);
+
   //display the random number
   diceEl.classList.remove("hidden");
+  diceEl.src = `/images/dice-${dice}.png`;
 
   //Check the number is one if true,switch to next player
 });
