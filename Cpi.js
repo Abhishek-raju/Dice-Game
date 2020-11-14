2f724e0832d99b592a4ab55a9b65b507d0180e36
Abhi Projects <cool.abhishekerketta@gmail.com>
2020-11-14 11:11:02 +0530

    changes

---
+++ Cpi.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//Selceting Elements
+const score0 = document.querySelector("#score--0");
+const score1 = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+
+//Starting Conditions
+score0.textContent = "0";
+score1.textContent = "0";
+
+
+//Rolling Dice Functionality
+btnrolldice.addEventListener("click", function () {
+    //Generation a random number between 1-6
+    const dice=Math.random*
+    //display the random number
+
+    //Check the number is one if true,switch to next player
+});
