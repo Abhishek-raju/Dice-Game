b519f162111c3b521064bbbd387cc812a3ea1ad3
Abhi Projects <cool.abhishekerketta@gmail.com>
2020-11-14 11:20:20 +0530

    Rolling dice -Random num Generation

--- Cpi.js
+++ Cpi.js
@@ -9,17 +9,18 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-
 //Starting Conditions
 score0.textContent = "0";
-score1.textContent = "0";
-
+score1.textContent = "1";
+diceEl.classList.add("hidden");
 
 //Rolling Dice Functionality
-btnrolldice.addEventListener("click", function () {
-    //Generation a random number between 1-6
-    const dice=Math.random*
-    //display the random number
+btnRoll.addEventListener("click", function () {
+  //Generation a random number between 1-6
+  const dice = Math.floor(Math.random() * 6) + 1;
+  console.log(dice);
+  //display the random number
+  diceEl.classList.remove("hidden");
 
-    //Check the number is one if true,switch to next player
+  //Check the number is one if true,switch to next player
 });
